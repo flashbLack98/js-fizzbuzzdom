0cd110aaef10130efae17c40716b01f61a6203f7
flashbLack98 <npalakuttige98@icloud.com>
2021-10-12 16:29:52 +0200

    celle

--- js/script.js
+++ js/script.js
@@ -1,15 +1,24 @@
 let scacchiera = document.getElementById("container_scacchiera");
 
-
-let row = document.createElement("div");
-row.classList.add("row");
-scacchiera.append(row);
+for (let x = 1; x <= 10; x++) {
 
 
-let col = document.createElement("div");
-col.classList.add("col");
-row.append(col);
+    let row = document.createElement("div");
+    row.classList.add("row", "m-3");
+    scacchiera.append(row);
 
-let cella = document.createElement("div");
-cella.classList.add("ratio", "ratio-1x1", "bg-danger");
-col.append(cella);
+    for (let i = 1; i <= 7; i++) {
+
+        let col = document.createElement("div");
+        col.classList.add("col");
+        row.append(col);
+
+
+
+        let cella = document.createElement("div");
+        cella.classList.add("ratio", "ratio-1x1", "bg-primary");
+        col.append(cella);
+        cella.textContent = i;
+
+    }
+}
